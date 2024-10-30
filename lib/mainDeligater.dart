60960065f7ac0c153351ff969e60250a5967f544
Rerurate514 <rerurateyuto@gmail.com>
2024-10-30 22:13:13 +0900

    mianDeligatorの作成

--- lib/mainDeligater.dart
+++ lib/mainDeligater.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:obsididget/pages/HomePage.dart';
 
 class MainDeligater extends StatelessWidget {
+  const MainDeligater({super.key});
+
   @override
   Widget build(BuildContext context){
-    return Container();
+    return SafeArea(
+      child: Center(
+        child: HomePage(),
+      )
+    );
   }
 }
